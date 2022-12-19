51d55354baea1bfa2ae0c1bd1870f6ff5a95ecba
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-19 14:50:29 -0300

    Adding Statistic logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,69 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import randomWords from "random-words"
 const NUMB_OF_WORDS = 200
-const SECONDS = 60
+const SECONDS = 10
 
 function App() {
 	const [words, setWords] = useState([])
 	const [countDown, setCountDown] = useState(SECONDS)
 	const [currInput, setCurrInput] = useState("")
 	const [currWordIndex, setCurrWordIndex] = useState(0)
+	const [correct, setCorrect] = useState(0)
+	const [incorrect, setIncorrect] = useState(0)
+	const [status, setStatus] = useState("waiting")
+	const textInput = useRef(null)
 
 	useEffect(() => {
 		setWords(generateWords())
 	}, [])
+
+	useEffect(() => {
+		if (status === "started") {
+			textInput.current.focus()
+		}
+	})
+
 	const generateWords = () => {
 		return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords())
 	}
 	const start = () => {
-		let interval = setInterval(() => {
-			setCountDown((prevCountDown) => {
-				if (prevCountDown === 0) {
-					clearInterval(interval)
-				} else {
-					return prevCountDown - 1
-				}
-			})
-		}, 1000)
+		if (status === "finished") {
+			setWords(generateWords())
+			setCurrWordIndex(0)
+			setCorrect(0)
+			setIncorrect(0)
+		}
+		if (status !== "started") {
+			setStatus("started")
+			let interval = setInterval(() => {
+				setCountDown((prevCountDown) => {
+					if (prevCountDown === 0) {
+						clearInterval(interval)
+						setStatus("finished")
+						setCurrInput("")
+						return SECONDS
+					} else {
+						return prevCountDown - 1
+					}
+				})
+			}, 1000)
+		}
 	}
 	const handleKeyDown = ({ keyCode }) => {
 		if (keyCode == 32) {
 			checkMatch()
+			setCurrInput("")
 			setCurrWordIndex(currWordIndex + 1)
 		}
 	}
 	const checkMatch = () => {
 		const wordToCompare = words[currWordIndex]
 		const doesItMatch = wordToCompare === currInput.trim()
-		console.log({ doesItMatch })
+		if (doesItMatch) {
+			setCorrect(correct + 1)
+		} else {
+			setIncorrect(incorrect + 1)
+		}
 	}
 
 	return (
@@ -47,6 +75,8 @@ function App() {
 			</div>
 			<div className='control is-expanded section'>
 				<input
+					ref={textInput}
+					disabled={status !== "started"}
 					type='text'
 					className='input'
 					onKeyDown={handleKeyDown}
@@ -59,24 +89,42 @@ function App() {
 					Start
 				</button>
 			</div>
-			<div className='section'>
-				<div className='card'>
-					<div className='card-content'>
-						<div className='content'>
-							{words.map((word, i) => (
-								<span key={i}>
-									<span>
-										{word.split("").map((char, idx) => (
-											<span key={idx}>{char}</span>
-										))}
+			{status === "started" && (
+				<div className='section'>
+					<div className='card'>
+						<div className='card-content'>
+							<div className='content'>
+								{words.map((word, i) => (
+									<span key={i}>
+										<span>
+											{word.split("").map((char, idx) => (
+												<span key={idx}>{char}</span>
+											))}
+										</span>
+										<span> </span>
 									</span>
-									<span> </span>
-								</span>
-							))}
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			)}
+			{status === "finished" && (
+				<div className='section'>
+					<div className='columns'>
+						<div className='column has-text-centered'>
+							<p className='is-size-5'>Words per minute:</p>
+							<p className='has-text-primary is-size-1'>{correct}</p>
+						</div>
+						<div className='column has-text-centered'>
+							<div className='is-size-5'>Accuracy:</div>
+							<p className='has-text-info is-size-1'>
+								{Math.round((correct / (correct + incorrect)) * 100)} %
+							</p>
+						</div>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
