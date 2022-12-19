6ff89e2d9bd100c6444358af2812405509bd5941
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-19 15:39:28 -0300

    Adding highlight to the current words

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState, useRef } from "react"
 import randomWords from "random-words"
 const NUMB_OF_WORDS = 200
-const SECONDS = 10
+const SECONDS = 60
 
 function App() {
 	const [words, setWords] = useState([])
 	const [countDown, setCountDown] = useState(SECONDS)
 	const [currInput, setCurrInput] = useState("")
 	const [currWordIndex, setCurrWordIndex] = useState(0)
+	const [currCharIndex, setCurrCharIndex] = useState(-1)
+	const [currChar, setCurrChar] = useState("")
 	const [correct, setCorrect] = useState(0)
 	const [incorrect, setIncorrect] = useState(0)
 	const [status, setStatus] = useState("waiting")
@@ -21,7 +23,7 @@ function App() {
 		if (status === "started") {
 			textInput.current.focus()
 		}
-	})
+	}, [status])
 
 	const generateWords = () => {
 		return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords())
@@ -32,6 +34,8 @@ function App() {
 			setCurrWordIndex(0)
 			setCorrect(0)
 			setIncorrect(0)
+			setCurrCharIndex(-1)
+			setCurrChar("")
 		}
 		if (status !== "started") {
 			setStatus("started")
@@ -49,11 +53,18 @@ function App() {
 			}, 1000)
 		}
 	}
-	const handleKeyDown = ({ keyCode }) => {
+	const handleKeyDown = ({ keyCode, key }) => {
 		if (keyCode == 32) {
 			checkMatch()
 			setCurrInput("")
 			setCurrWordIndex(currWordIndex + 1)
+			setCurrCharIndex(-1)
+		} else if (keyCode === 8) {
+			setCurrCharIndex(currCharIndex - 1)
+			setCurrChar("")
+		} else {
+			setCurrCharIndex(currCharIndex + 1)
+			setCurrChar(key)
 		}
 	}
 	const checkMatch = () => {
@@ -63,6 +74,27 @@ function App() {
 			setCorrect(correct + 1)
 		} else {
 			setIncorrect(incorrect + 1)
+		}
+	}
+	const getCharClass = (wordIdx, charIdx, char) => {
+		if (
+			wordIdx === currWordIndex &&
+			charIdx === currCharIndex &&
+			currChar &&
+			status !== "finished"
+		) {
+			if (char === currChar) {
+				return "has-background-success"
+			} else {
+				return "has-background-danger"
+			}
+		} else if (
+			wordIdx === currWordIndex &&
+			currCharIndex >= words[currWordIndex].length
+		) {
+			return "has-background-danger"
+		} else {
+			return ""
 		}
 	}
 
@@ -98,7 +130,9 @@ function App() {
 									<span key={i}>
 										<span>
 											{word.split("").map((char, idx) => (
-												<span key={idx}>{char}</span>
+												<span className={getCharClass(i, idx, char)} key={idx}>
+													{char}
+												</span>
 											))}
 										</span>
 										<span> </span>
